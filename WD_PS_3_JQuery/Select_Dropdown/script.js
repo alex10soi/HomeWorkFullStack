49812846315c690e939157178e21fe3acdf4771a
Alex Saiko <alexustassoi@gmail.com>
2020-03-20 14:18:34 +0000

    Added click outside Drop Down zone(fold it)

--- WD_PS_3_JQuery/Select_Dropdown/script.js
+++ WD_PS_3_JQuery/Select_Dropdown/script.js
@@ -1,38 +1,38 @@
 const SELECT_DATA = [{
-      name: 'Select',
-      images: 'images/filtering_1.png'
+    name: 'Select',
+    images: 'images/filtering_1.png'
   },
   {
-      name: 'USA',
-      images: 'images/icons8-usa-48.png'
+    name: 'USA',
+    images: 'images/icons8-usa-48.png'
   },
   {
-      name: 'Latvia',
-      images: 'images/icons8-latvia-48.png'
+    name: 'Latvia',
+    images: 'images/icons8-latvia-48.png'
   },
   {
-      name: 'Netherlands',
-      images: 'images/icons8-netherlands-48.png'
+    name: 'Netherlands',
+    images: 'images/icons8-netherlands-48.png'
   },
   {
-      name: 'Poland',
-      images: 'images/icons8-poland-48.png'
+    name: 'Poland',
+    images: 'images/icons8-poland-48.png'
   },
   {
-      name: 'Romania',
-      images: 'images/icons8-romania-48.png'
+    name: 'Romania',
+    images: 'images/icons8-romania-48.png'
   },
   {
-      name: 'Spain',
-      images: 'images/icons8-spain-48.png'
+    name: 'Spain',
+    images: 'images/icons8-spain-48.png'
   },
   {
-      name: 'Thailand',
-      images: 'images/icons8-thailand-48.png'
+    name: 'Thailand',
+    images: 'images/icons8-thailand-48.png'
   },
   {
-      name: 'Ukraine',
-      images: 'images/icons8-ukraine-48.png'
+    name: 'Ukraine',
+    images: 'images/icons8-ukraine-48.png'
   }
 ];
 
@@ -55,46 +55,49 @@ $(document).ready(function() {
 
   // Collapses Drop Down after selecting an item and changes the values ​​in the title 
   //item to the values ​​of the selected item.
-  $('#select_item_0').click(()=> {
-    if(flag){
+  $('#select_item_0').click(() => {
+    if (flag) {
       drop_down_list.slideDown();
       $('.fa-angle-down').addClass('rotate_icon');
-      flag = !flag; 
-    }else{
+      flag = !flag;
+    } else {
       drop_down_list.slideUp();
       $('.fa-angle-down').removeClass('rotate_icon');
-      flag = !flag; 
+      flag = !flag;
     }
   });
 
-  $('.select_item_list').click((event)=> {
+  $('.select_item_list').click((event) => {
     const src = $('img[id="' + event.target.id + '"').attr('src');
     const spanValue = $('span[id="' + event.target.id + '"')[0].innerText;
-   
+
     $('img[id=select_item_0').attr('src', src);
     $('span[id=select_item_0').text(spanValue);
     drop_down_list.slideUp();
     $('.fa-angle-down').removeClass('rotate_icon');
-    flag = !flag; 
+    flag = !flag;
   });
 
-  
-  // Collapses Drop Down when the user leaves the Drop Down area of ​​the sheet
-  select_wrapper.mouseleave(()=> {
-    drop_down_list.slideUp();
-    $('.fa-angle-down').removeClass('rotate_icon');
-    flag = !flag; 
+
+  // Collapses the Drop Down list when a user clicks outside the area of ​​the Drop Down block
+  $(document).mouseup(function(e) {
+    var container = $("#drop_down_main_wrapper");
+    if (container.has(e.target).length === 0) {
+      drop_down_list.slideUp();
+      $('.fa-angle-down').removeClass('rotate_icon');
+      flag = !flag;
+    }
   });
 
 
   // Changes the color of items in the list when you hover over an item and leave its area.
   $(".select_item, .select_item_list").hover(
-    (item)=> {
+    (item) => {
       $("#" + item.currentTarget.id).css({
         "background-color": "rgb(124, 186, 226, 0.2)"
       });
     },
-    (item)=> {
+    (item) => {
       $("#" + item.currentTarget.id).css({
         "background-color": "rgba(171, 184, 142, 0.5)",
       });
